502670c4c3c81e945ce6782c0b668b12b393c1bc
icmoore <ic3moore@gmail.com>
2024-05-13 16:02:12 -0700

    RebaseIndexToken bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='DeFiPy',
-      version='0.0.22',
+      version='0.0.23',
       description='DeFi Analytics with Python',
       long_description=long_description,
       long_description_content_type="text/markdown",
